8dfc2f4ffc9e12bd61fc13724d1f6a4bd404fc7b
nearias3 <127260408+nearias3@users.noreply.github.com>
2024-09-25 16:40:13 -0500

    Completed the logic for storing and retrieving content from the database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,41 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+  const db = await openDB("jate", 1);
+  const tx = db.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, content });
+
+  const result = await request;
+  
+  console.log("Content saved to the database", result);
+
+  } catch (error) {
+    console.error("putDb not implemented", error);
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try {
+    const db = await openDB('jate', 1);
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(1);
+    const result = await request;
+
+    if (result) {
+      console.log('Content retrieved from the database', result.content);
+      return result.content;
+  } else {
+    console.log('No content found in the database');
+    return null;
+  }
+} catch(error) {
+  console.error('getDb not implemented');
+}
+};
 
 initdb();
